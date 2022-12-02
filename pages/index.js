d4a26b8298518c260d7a86f80728983f736a2f5e
hazratamin98 <hazratamin98@gmail.com>
2022-12-02 15:30:37 +0500

    setup

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return <div className="bg-orange-600">Hello from shopify dev</div>;
+}
